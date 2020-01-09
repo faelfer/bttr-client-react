96481b6fb712ddb733e6d70aace32751d73efa3d
NYDino <rfapp00@gmail.com>
2020-01-09 13:20:06 -0300

    :sparkles: tasks is working

--- src/pages/Tasks/components/ChackBox/index.js
+++ src/pages/Tasks/components/ChackBox/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import "./styles.css";
 
-function CheckBox({ handleCheckChieldElement, task }) {
+function CheckBox({ handleCheckChieldElement, task, key }) {
   return (
     <li>
+      <label class="container">{task.name}
       <input 
-        key={task.id} 
+        key={key} 
         onClick={handleCheckChieldElement} 
         type="checkbox" 
         checked={task.done} 
         value={task.name} 
-      /> 
-        {task.name}
+      />
+        <span class="checkmark"></span>
+      </label>
    </li>
   )
 };
