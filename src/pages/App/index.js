3ff6cfd75ab8671580c6894dcbacc1ba7616d85c
NYDino <rfapp00@gmail.com>
2020-02-06 13:02:00 -0300

    update 06/02

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -8,11 +8,11 @@ import Load from "../../components/Load";
 
 function App({ history }) {
     const [listCards, setListCards] = useState([
-        {"name": "Code", "goalPerDay": 70, "goalDone": (70 + 70), "icon": "fas fa-code"},    
+        {"name": "Code", "goalPerDay": 70, "goalDone": (70 + 70 + 74), "icon": "fas fa-code"},    
         {"name": "Vest", "goalPerDay": 80, "goalDone": (80 + 80), "icon": "fas fa-university"},
         {"name": "Draw", "goalPerDay": 15, "goalDone": (0), "icon": "fas fa-pencil-ruler"},   
         {"name": "Book", "goalPerDay": 20, "goalDone": (0), "icon": "fas fa-book-open"},
-        {"name": "Care", "goalPerDay": 30, "goalDone": (30 + 30), "icon": "fas fa-spa"}
+        {"name": "Care", "goalPerDay": 30, "goalDone": (30 + 30 + 30), "icon": "fas fa-spa"}
     ]);
     const [load, setLoad] = useState(false);
     const token = getToken(); 
