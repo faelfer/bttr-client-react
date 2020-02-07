ca71c03a8fa6755f1afdc06f7325f34ee81586a9
NYDino <rfapp00@gmail.com>
2020-02-06 22:36:24 -0300

    update 06/02 | 3x

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -9,7 +9,7 @@ import Load from "../../components/Load";
 function App({ history }) {
     const [listCards, setListCards] = useState([
         {"name": "Code", "goalPerDay": 70, "goalDone": (70 + 70 + 74 + 69), "icon": "fas fa-code"},    
-        {"name": "Vest", "goalPerDay": 80, "goalDone": (80 + 80), "icon": "fas fa-university"},
+        {"name": "Vest", "goalPerDay": 80, "goalDone": (80 + 80 + 70 + 85), "icon": "fas fa-university"},
         {"name": "Draw", "goalPerDay": 15, "goalDone": (0), "icon": "fas fa-pencil-ruler"},   
         {"name": "Book", "goalPerDay": 20, "goalDone": (0), "icon": "fas fa-book-open"},
         {"name": "Care", "goalPerDay": 30, "goalDone": (30 + 30 + 30), "icon": "fas fa-spa"}
