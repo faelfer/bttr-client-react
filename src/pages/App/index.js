70099605677f780786de8b3ab8749210d7d0293f
NYDino <rfapp00@gmail.com>
2020-02-27 13:18:17 -0300

    update 27/02

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -8,11 +8,11 @@ import Load from "../../components/Load";
 
 function App({ history }) {
     const [listCards, setListCards] = useState([
-        {"name": "Code", "goalPerDay": 70, "goalDone": (70 + 70 + 74 + 69 + 67 + 63 + 70 + 7 + 70 + 70 + 70 + 12), "icon": "fas fa-code"},    
+        {"name": "Code", "goalPerDay": 70, "goalDone": (70 + 70 + 74 + 69 + 67 + 63 + 70 + 7 + 70 + 70 + 70 + 12 + 70 + 70), "icon": "fas fa-code"},    
         {"name": "Vest", "goalPerDay": 80, "goalDone": (80 + 80 + 70 + 85 + 70 + 30), "icon": "fas fa-university"},
         {"name": "Draw", "goalPerDay": 15, "goalDone": (0), "icon": "fas fa-pencil-ruler"},   
         {"name": "Book", "goalPerDay": 20, "goalDone": (336), "icon": "fas fa-book-open"},
-        {"name": "Care", "goalPerDay": 30, "goalDone": (30 + 30 + 30 + 30 + 30 + 30 + 30 + 45), "icon": "fas fa-spa"},
+        {"name": "Care", "goalPerDay": 30, "goalDone": (30 + 30 + 30 + 30 + 30 + 30 + 30 + 45 + 45), "icon": "fas fa-spa"},
         {"name": "Burn", "goalPerDay": 15, "goalDone": (0), "icon": "fas fa-fire-alt"}
     ]);
     const [load, setLoad] = useState(false);
