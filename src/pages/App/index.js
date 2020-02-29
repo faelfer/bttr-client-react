3a13fffab2ef87776b3ed66de1173199861017b3
NYDino <rfapp00@gmail.com>
2020-02-28 23:35:50 -0300

    update 28/02 | 6x

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -12,7 +12,7 @@ function App({ history }) {
         {"name": "Vest", "goalPerDay": 80, "goalDone": (80 + 80 + 70 + 85 + 70 + 30), "icon": "fas fa-university"},
         {"name": "Draw", "goalPerDay": 15, "goalDone": (0), "icon": "fas fa-pencil-ruler"},   
         {"name": "Book", "goalPerDay": 20, "goalDone": (336), "icon": "fas fa-book-open"},
-        {"name": "Care", "goalPerDay": 30, "goalDone": (30 + 30 + 30 + 30 + 30 + 30 + 30 + 45 + 45), "icon": "fas fa-spa"},
+        {"name": "Care", "goalPerDay": 30, "goalDone": (30 + 30 + 30 + 30 + 30 + 30 + 30 + 45 + 45 + 33), "icon": "fas fa-spa"},
         {"name": "Burn", "goalPerDay": 30, "goalDone": (95), "icon": "fas fa-fire-alt"}
     ]);
     const [load, setLoad] = useState(false);
