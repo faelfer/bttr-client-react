4720793ad7b9df7937cf9ff8b78f1555acb6a183
NYDino <rfapp00@gmail.com>
2020-03-10 00:08:13 -0300

    update 09/03

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -10,7 +10,7 @@ import Modal from "./components/Modal";
 function App({ history }) {
     const [listCards, setListCards] = useState([
         {"name": "Code", "goalPerDay": 70, "goalDone": (70 + 81 + 81 + 76 + 72 + 5), "icon": "fas fa-code"},    
-        {"name": "Vest", "goalPerDay": 80, "goalDone": (0), "icon": "fas fa-university"},
+        {"name": "Vest", "goalPerDay": 80, "goalDone": (103), "icon": "fas fa-university"},
         {"name": "Draw", "goalPerDay": 15, "goalDone": (0), "icon": "fas fa-pencil-ruler"},   
         {"name": "Book", "goalPerDay": 20, "goalDone": (0), "icon": "fas fa-book-open"},
         {"name": "Care", "goalPerDay": 30, "goalDone": (0), "icon": "fas fa-spa"},
