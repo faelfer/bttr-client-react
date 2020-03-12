c7309ffc9c15a5feca231b02a21fcd1d8fbf07a7
NYDino <rfapp00@gmail.com>
2020-03-12 01:10:41 -0300

    update 11/03

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -10,10 +10,10 @@ import Modal from "./components/Modal";
 function App({ history }) {
     const [listCards, setListCards] = useState([
         {"name": "Code", "goalPerDay": 70, "goalDone": (70 + 81 + 81 + 76 + 72 + 5 + 72 + 33 + 70), "icon": "fas fa-code"},    
-        {"name": "Vest", "goalPerDay": 80, "goalDone": (103), "icon": "fas fa-university"},
+        {"name": "Vest", "goalPerDay": 80, "goalDone": (103 + 70 + 80), "icon": "fas fa-university"},
         {"name": "Draw", "goalPerDay": 15, "goalDone": (0), "icon": "fas fa-pencil-ruler"},   
         {"name": "Book", "goalPerDay": 20, "goalDone": (27 + 25), "icon": "fas fa-book-open"},
-        {"name": "Care", "goalPerDay": 30, "goalDone": (0), "icon": "fas fa-spa"},
+        {"name": "Care", "goalPerDay": 30, "goalDone": (30 + 30 + 60), "icon": "fas fa-spa"},
         {"name": "Burn", "goalPerDay": 30, "goalDone": (14), "icon": "fas fa-fire-alt"}
     ]);
     const [isLoad, setIsLoad] = useState(false);
