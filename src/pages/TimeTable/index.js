ae0f545b59e363002e62b60f4a824da7172e5ec9
faelfer <rfapp00@gmail.com>
2021-03-23 02:09:48 -0300

    :sparkles: date filter screen time

--- src/pages/TimeTable/index.js
+++ src/pages/TimeTable/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./styles.css";
 import api from "../../services/api";
 import { getToken, logout } from "../../services/auth";
@@ -12,59 +12,60 @@ export default function TimeTable({ history }) {
     const { abiliityId } = useParams();
     const [times, setTimes] = useState([]);
     const [isLoad, setIsLoad] = useState(false);
+    const [dateFilter, setDateFilter] = useState("any date");
     const [error, setError] = useState('');
 
+    const getTimesFilterByAbiliity = useCallback(async () => {
+      setIsLoad(true);
+        try {
+          const response = await api.get(`/time/filter_by_abiliity/${abiliityId}`, {
+              headers: { "Authorization": token }
+          });
+          console.log("getTimes | response: ", response);
+          setIsLoad(false);
+          if(!response.data.status === 200) {
+              setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
+          }
+
+          setTimes(response.data)
+        } catch (error) {
+          console.log("getTimes | error: ", error);
+            if(error.message === "Request failed with status code 401") {
+              logout();
+              history.push("/");
+            }
+          setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
+          setIsLoad(false);
+        }
+
+    }, [token, history, abiliityId]);
+
+    const getTimes = useCallback(async () => {
+      setIsLoad(true);
+        try {
+          const response = await api.get('/time', {
+              headers: { "Authorization": token }
+          });
+          console.log("getTimes | response: ", response);
+          setIsLoad(false);
+          if(!response.data.status === 200) {
+              setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
+          }
+
+          setTimes(response.data)
+        } catch (error) {
+          console.log("getTimes | error: ", error);
+            if(error.message === "Request failed with status code 401") {
+              logout();
+              history.push("/");
+            }
+          setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
+          setIsLoad(false);
+        }
+
+    }, [token, history]);
+
     useEffect(() => {
-      console.log(abiliityId)
-      async function getTimesFilterByAbiliity() {
-        setIsLoad(true);
-          try {
-            const response = await api.get(`/time/filter_by_abiliity/${abiliityId}`, {
-                headers: { "Authorization": token }
-            });
-            console.log("getTimes | response: ", response);
-            setIsLoad(false);
-            if(!response.data.status === 200) {
-                setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
-            }
-
-            setTimes(response.data)
-          } catch (error) {
-            console.log("getTimes | error: ", error);
-              if(error.message === "Request failed with status code 401") {
-                logout();
-                history.push("/");
-              }
-            setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
-            setIsLoad(false);
-          }
-
-      };
-
-      async function getTimes() {
-        setIsLoad(true);
-          try {
-            const response = await api.get('/time', {
-                headers: { "Authorization": token }
-            });
-            console.log("getTimes | response: ", response);
-            setIsLoad(false);
-            if(!response.data.status === 200) {
-                setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
-            }
-
-            setTimes(response.data)
-          } catch (error) {
-            console.log("getTimes | error: ", error);
-              if(error.message === "Request failed with status code 401") {
-                logout();
-                history.push("/");
-              }
-            setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
-            setIsLoad(false);
-          }
-
-      };
 
       if(!abiliityId) {
         getTimes();
@@ -72,7 +73,48 @@ export default function TimeTable({ history }) {
         getTimesFilterByAbiliity();
       }
 
+    }, [token, history, abiliityId, getTimes, getTimesFilterByAbiliity]);
+
+    const getTimesFilterByMonth = useCallback(async () => {
+      setIsLoad(true);
+        try {
+          const response = await api.get(`/time/historic_month/${abiliityId}`, {
+              headers: { "Authorization": token }
+          });
+          console.log("getTimes | response: ", response);
+          setIsLoad(false);
+          if(!response.data.status === 200) {
+              setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
+          }
+
+          setTimes(response.data)
+        } catch (error) {
+          console.log("getTimes | error: ", error);
+            if(error.message === "Request failed with status code 401") {
+              logout();
+              history.push("/");
+            }
+          setError("Houve um problema ao listar as habilidades, tente novamente mais tarde");
+          setIsLoad(false);
+        }
+
     }, [token, history, abiliityId]);
+
+    useEffect(() => {
+      switch (dateFilter) {
+        case 'month':
+          getTimesFilterByMonth()
+          break;
+        default:
+          if(!abiliityId) {
+            getTimes();
+          } else {
+            getTimesFilterByAbiliity();
+          }
+      }
+
+
+    }, [abiliityId, dateFilter, getTimes, getTimesFilterByAbiliity, getTimesFilterByMonth]);
 
     return (
         <>
@@ -83,10 +125,29 @@ export default function TimeTable({ history }) {
                 <Load isShow={isLoad}/>
                 <>
                   <div className="time__create">
-                    <button className="form__button" onClick={() => history.push("/time")}>
+                    <button className="time__button" onClick={() => history.push("/time")}>
                       Criar registro de tempo
                     </button>  
                   </div>
+
+                  {abiliityId ?
+                      (
+                        <select 
+                          className="time__filter" 
+                          value={dateFilter} 
+                          onChange={event => {
+                            console.log("select | event.target.value: ",event.target.value)
+                            setDateFilter(event.target.value) 
+                          }}
+                        >
+                          <option value="month">Este mês</option>
+                          <option value="any date">Qualquer data</option>
+                        </select>
+                      )
+                    :
+                      null
+                  }
+
                   {times.map((time, key) => (
                     <Time time={time} history={history} key={key}/>
                   ))}
