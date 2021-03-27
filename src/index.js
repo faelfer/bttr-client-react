4eb1af094ddcf6cd1bd7c7ec44b62705133dfaf6
faelfer <rfapp00@gmail.com>
2021-03-27 15:20:38 -0300

    :sparkle: sentry added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
+
+Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY,
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API}`
+  baseURL: process.env.REACT_APP_API
 });
 
 api.interceptors.request.use(async config => {
