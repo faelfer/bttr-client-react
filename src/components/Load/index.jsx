9d56be64b1daa4bb8ea3cdeaec2f5ee69eea874f
faelfer <rfapp00@gmail.com>
2023-06-19 00:06:18 -0300

    added eslint

---
+++ src/components/Load/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './styles.css';
+
+function Load({ isShow }) {
+  return (
+    <div className={isShow ? 'load display-flex' : 'load display-none'}>
+      <div className="loadingio-spinner-dual-ring-ni6kc8xkbs">
+        <div className="ldio-e48qmwg845o">
+          <div />
+          <div>
+            <div />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Load;
