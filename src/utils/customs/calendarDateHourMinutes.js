f04d16b2f3b41c50144c9d11af995c05f8214318
faelfer <rfapp00@gmail.com>
2023-06-22 20:53:03 -0300

    added tests units to validade password and email

--- src/utils/customs/calendarDateHourMinutes.js
+++ src/utils/customs/calendarDateHourMinutes.js
@@ -1,24 +1,23 @@
 import { DateTime } from 'luxon';
 
 export default function calendarDateHourMinutes(dateString) {
-  try {
-    console.log('calendarDateHourMinutes | dateString: ', dateString);
+  // console.log('calendarDateHourMinutes | dateString: ', dateString);
 
-    const dateTimeFromISO = DateTime.fromISO(dateString);
-    console.log('calendarDateHourMinutes | dateTimeFromISO: ', dateTimeFromISO);
+  const dateTimeFromISO = DateTime.fromISO(dateString);
+  // console.log('calendarDateHourMinutes | dateTimeFromISO: ', dateTimeFromISO);
 
-    const dateCalendarFormat = dateTimeFromISO.toLocaleString(DateTime.DATE_SHORT);
-    console.log('calendarDateHourMinutes | dateCalendarFormat: ', dateCalendarFormat);
+  const dateCalendarFormat = dateTimeFromISO.toLocaleString(DateTime.DATE_SHORT);
+  // console.log('calendarDateHourMinutes | dateCalendarFormat: ', dateCalendarFormat);
 
-    const timeCalendarFormat = dateTimeFromISO.toLocaleString(DateTime.TIME_24_SIMPLE);
-    console.log('calendarDateHourMinutes | timeCalendarFormat: ', timeCalendarFormat);
-
-    const datetimeCalendarFormat = `${dateCalendarFormat} às ${timeCalendarFormat}`;
-    console.log('calendarDateHourMinutes | datetimeCalendarFormat: ', datetimeCalendarFormat);
-
-    return datetimeCalendarFormat;
-  } catch (error) {
-    console.log('calendarDateHourMinutes | error: ', error);
+  if (dateCalendarFormat === 'Invalid DateTime') {
     return '00/00/0000 às 00:00';
   }
+
+  const timeCalendarFormat = dateTimeFromISO.toLocaleString(DateTime.TIME_24_SIMPLE);
+  // console.log('calendarDateHourMinutes | timeCalendarFormat: ', timeCalendarFormat);
+
+  const datetimeCalendarFormat = `${dateCalendarFormat} às ${timeCalendarFormat}`;
+  // console.log('calendarDateHourMinutes | datetimeCalendarFormat: ', datetimeCalendarFormat);
+
+  return datetimeCalendarFormat;
 }
