eb08a5b9b9f8ec3afc4819dca50a9ab5fb865275
faelfer <rfapp00@gmail.com>
2023-09-01 21:48:31 -0300

    fix tests in skill factory

--- e2e/factories/__tests__/skillFactory.test.js
+++ e2e/factories/__tests__/skillFactory.test.js
@@ -10,25 +10,21 @@ test('deve retornar um objeto com os dados de uma habilidade padrão', () => {
 test('deve verificar as propriedades do objeto habilidade padrão', async () => {
   const skillDefault = await skillFactory(false);
   expect(skillDefault).toHaveProperty('name');
-  expect(skillDefault).toHaveProperty('dailyMinutes');
 });
 
 test('deve verificar os tipos do objeto habilidade padrão', async () => {
   const skillDefault = await skillFactory(false);
   expect(typeof skillDefault).toBe('object');
   expect(typeof skillDefault.name).toBe('string');
-  expect(typeof skillDefault.dailyMinutes).toBe('string');
 });
 
 test('deve verificar as propriedades do objeto habilidade novo', async () => {
   const skillNew = await skillFactory();
   expect(skillNew).toHaveProperty('name');
-  expect(skillNew).toHaveProperty('dailyMinutes');
 });
 
 test('deve verificar os tipos do objeto habilidade novo', async () => {
   const skillNew = await skillFactory();
   expect(typeof skillNew).toBe('object');
   expect(typeof skillNew.name).toBe('string');
-  expect(typeof skillNew.dailyMinutes).toBe('string');
 });
