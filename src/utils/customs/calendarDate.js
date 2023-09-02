728474b185d73e0d6e7cd9cd8850df02c68692c7
faelfer <rfapp00@gmail.com>
2023-09-01 22:21:27 -0300

    set time zone in calendar date hour minutes

--- src/utils/customs/calendarDate.js
+++ src/utils/customs/calendarDate.js
@@ -3,7 +3,7 @@ import { DateTime } from 'luxon';
 export default function calendarDate(dateString) {
   // console.log('calendarDate | dateString: ', dateString);
 
-  const dateTimeFromISO = DateTime.fromISO(dateString);
+  const dateTimeFromISO = DateTime.fromISO(dateString).setLocale('pt-BR');
   // console.log('calendarDate | dateTimeFromISO: ', dateTimeFromISO);
 
   const dateCalendarFormat = dateTimeFromISO.toLocaleString(DateTime.DATE_SHORT);
