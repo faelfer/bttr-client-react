ff29fbc2ef4119b02f8044be8276a342a3887b37
faelfer <rfapp00@gmail.com>
2023-09-25 23:16:48 -0300

    review rules function to ts

---
+++ src/utils/showToast.ts
@@ -0,0 +1,18 @@
+import { createStandaloneToast } from "@chakra-ui/react";
+
+const { toast } = createStandaloneToast();
+
+export default function showToast(
+  title: string,
+  description: string,
+  status: "info" | "warning" | "success" | "error" | "loading",
+): void {
+  toast({
+    position: "top",
+    title,
+    description,
+    status,
+    duration: 3000,
+    isClosable: false,
+  });
+}
